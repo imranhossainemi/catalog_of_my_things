11a4a8c67cc2806b58bf282ace45a757183b0767
ikoote1 <ikootepreim@gmail.com>
2023-06-06 11:57:42 +0300

    game and author

--- author.rb
+++ author.rb
@@ -1,4 +1,36 @@
 require_relative item
 class Author < Item
+    attr_reader :id, :first_name, :last_name, :_items
+    def initialize(first_name = '', last_name = '', _items = [])
+        super()
+        @id_counter |= 0
+        @id_counter += 1
+        @id = @id_counter
+        @first_name = first_name
+        @last_name = last_name
+        @items = []
+    end
+
+    def add_item(item)
+        @items << item unless @items.include?(item)
+        item.add_author(self)
+      end
     
+      def full_name
+        "#{@first_name} #{@last_name}"
+      end
+    
+      def to_hash
+        {
+          id: @id,
+          first_name: @first_name,
+          last_name: @last_name,
+          items: @items.map(&:to_hash)
+        }
+      end
+
+    class << self
+        attr_accessor :id_counter
+      end
+      @id_counter = 0
 end
