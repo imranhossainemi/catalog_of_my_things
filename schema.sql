9275e628491686ca89e2905b18b1b65146dedb67
ikoote1 <ikootepreim@gmail.com>
2023-06-08 09:57:50 +0300

    schema.sql

--- schema.sql
+++ schema.sql
@@ -33,3 +33,47 @@ CREATE TABLE label (
 );
 
 CREATE INDEX idx_book_label_id ON book (label_id);
+
+--music album table --
+CREATE TABLE music_albums (
+  id SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL DEFAULT DATE,
+  on_spotify BOOLEAN,
+  archived BOOLEAN,
+  genre_id INT,
+  label_id INT,
+  author_id INT,
+  CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+  CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES label(label_id)
+  CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors(author_id)
+  PRIMARY KEY(id)
+);
+
+-- genre table --
+CREATE TABLE genres (
+  genre_id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  PRIMARY KEY(genre_id)
+);
+
+-- Create game table --
+CREATE TABLE game (
+  id SERIAL PRIMARY KEY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  genre_id INT,
+  label_id INT,
+  publish_date DATE,
+  author_id INT,
+  CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+  CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES label(label_id)
+  CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors(author_id)
+  PRIMARY KEY(id)
+);
+
+--author table --
+CREATE TABLE author (
+    id SERIAL PRIMARY KEY,
+    first_name  VARCHAR(100),
+    last_name   VARCHAR(100)
+);
