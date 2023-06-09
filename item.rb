73cd9eb2cd6692bbeb4e8718af5a91a394262e75
Imran Hossain <blur-imran@hotmail.com>
2023-06-09 11:26:28 +0600

    Update item.rb

--- item.rb
+++ item.rb
@@ -3,13 +3,13 @@ require 'date'
 class Item
   attr_reader :id, :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(id = Random.rand(1..1000),publish_date)
+  def initialize(id = Random.rand(1..1000))
     @id = id
     @genre = nil
     @author = nil
     @source = nil
     @label = nil
-    @publish_date = publish_date
+    @publish_date = nil
     @archived = false
   end
 
